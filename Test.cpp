e6c1aeba0434c517ac48bb96b4d078003f91da6d
imVinoth97 <79276096+imVinoth97@users.noreply.github.com>
2021-02-19 09:28:46 +0530

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-
+#include <conio.h>
 using namespace std;
-
-int main()
+int main ()
 {
-    cout << "Hello World" << endl;
+	char input[10] = "Welcome";
+    int no = strlen(input);
+    
+	for (int i=0;i<=no;i++)
+	{	for (int x=0;x<=no;x++)
+		{	if (x<=no-i)
+			{
+				cout<<" ";	
+			}
+			else
+			{
+				cout<<input[i/2];
+			}
+		}
+	
+	cout<<"\n";
+	}
+getch();
+return 0;
 }
+
 
